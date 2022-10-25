051f22597f69e499c27f1ac85ce344211fb248b7
Betty <chen.betty86@gmail.com>
2022-10-25 14:08:37 +0200

    update week 2 docker homework

--- homework/week_2/plot.py
+++ homework/week_2/plot.py
@@ -6,10 +6,11 @@ import pandas as pd
 converters = {
     "Original": lambda x: float(x.replace(",", "."))
 }
-df = pd.read_csv("./data/coding-environment-exercise.csv", index_col=0, encoding="utf-8", delimiter=";", converters=converters) 
+df = pd.read_csv("data/coding-environment-exercise.csv", index_col=0, encoding="utf-8", delimiter=";", converters=converters) 
 df["Original"] = df["Original"].astype(float)
 colors = sns.color_palette('pastel')[0:len(df.index.tolist())]
 plt.pie(df["Original"].tolist(), labels = df.index.tolist(), colors = colors, autopct='%1.1f%%')
 plt.title("Pringles Original Nutrition Percentage Pie Chart")
-plt.savefig("./plot-pringles.csv", format="png")
+plt.savefig("result/plot-pringles.png")
 plt.show()
+print("building successfully")
