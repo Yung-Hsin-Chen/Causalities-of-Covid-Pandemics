271600f0925afe8eb346d7a6a8f134d04fa6edec
Betty <chen.betty86@gmail.com>
2022-11-14 15:05:15 +0100

    save external dataset by make_dataset.py

---
+++ final_project/src/data/data_transform.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import requests
+import io
+
+def request_external_data():
+    url_lst = ["https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/new_deaths_per_million.csv",
+               "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv",
+               "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv",
+               "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations-by-manufacturer.csv",
+               "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations-by-age-group.csv"]
+    
+    for url in url_lst:
+
+        data = requests.get(url).content
+
+        # TURN REQUESTED DATA TO DATAFRAME
+        df_name = url[url.rfind("/")+1:url.rfind(".")]
+        df = pd.read_csv(io.StringIO(data.decode("utf-8")))
+        df.to_hdf("./data/external/"+df_name+".h5", key='df', mode='w') 
+
+    return
