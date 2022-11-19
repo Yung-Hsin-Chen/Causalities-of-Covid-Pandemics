9f80125759e34e8f6f61132a817d0317602904e8
Betty <chen.betty86@gmail.com>
2022-11-19 10:23:08 +0100

    save y_test_pred to models folder

--- final_project/src/models/predict_model.py
+++ final_project/src/models/predict_model.py
@@ -8,6 +8,7 @@ from sklearn.metrics import confusion_matrix
 from src.models.data_splitting import data_splitting
 import pickle
 import pandas as pd
+import numpy as np
 
 
 class Prediction:
@@ -31,18 +32,21 @@ class Prediction:
         model = pickle.load(open(os.path.join(os.path.abspath(""), "models", "model_lr.sav"), "rb"))
         y_test_pred = model.predict(self._X_test)
         self._model_lr_acc = self.get_accuracy(self._y_test, y_test_pred)
+        np.save(os.path.join(os.path.abspath(""), "models", "lr_y_test_pred.npy"), y_test_pred)
         return
 
     def predict_model_perc(self):
         model = pickle.load(open(os.path.join(os.path.abspath(""), "models", "model_perc.sav"), "rb"))
         y_test_pred = model.predict(self._X_test)
         self._model_lr_acc = self.get_accuracy(self._y_test, y_test_pred)
+        np.save(os.path.join(os.path.abspath(""), "models", "perc_y_test_pred.npy"), y_test_pred)
         return
 
     def predict_model_tree(self):
         model = pickle.load(open(os.path.join(os.path.abspath(""), "models", "model_tree.sav"), "rb"))
         y_test_pred = model.predict(self._X_test)
         self._model_lr_acc = self.get_accuracy(self._y_test, y_test_pred)
+        np.save(os.path.join(os.path.abspath(""), "models", "tree_y_test_pred.npy"), y_test_pred)
         return
 
     def get_model_accuracy(self):
